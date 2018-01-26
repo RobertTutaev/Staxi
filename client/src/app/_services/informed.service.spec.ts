0f508f6cb1451365c64ebb662cd3c4ceb594473a
RobertTutaev <rob_t@mail.ru>
2018-01-26 23:51:00 +0500

    1.3.1

--- client/src/app/_services/informed.service.spec.ts
+++ client/src/app/_services/informed.service.spec.ts
@@ -1,7 +1,7 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
-import { InformedService } from './Informed.service';
+import { InformedService } from './informed.service';
 
 describe('InformedService', () => {
   beforeEach(() => {
